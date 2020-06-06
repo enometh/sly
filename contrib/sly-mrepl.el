83f81fbd88c4baa95ae3d6856b20028a47a6d084
Madhu <enometh@net.meer>
2020-06-06 23:46:06 +0530

    C-c z sly-mrepl dwim behaviour
    With a prefix argument start a new connection:
    C-u     C-c z sly-mrepl - connect to localhost 4005
    M--     C-c z sly-mrepl - choose a lisp program to run
    C-u <n> C-c z sly-mrepl - prompt from sly-lisp-implementations
    
    Any other prefix argument will call sly-mrepl to switch to the first
    repl of the default connection. To handle the case when there is no
    lisp connection set sly-auto-start to 'ask
    
    * contrib/sly-mrepl.el: (sly-mrepl): add comment about sly-auto-start.
    (sly--read-interactive-args): r3. (sly-connect): r3. (sly-auto-start):
    r3.  (sly-mrepl-on-connection): don't delete a window when it is
    dedicated in its own frame.

--- contrib/sly-mrepl.el
+++ contrib/sly-mrepl.el
@@ -981,7 +981,15 @@ arglist for the most recently enclosed macro or function."
 If supplied, DISPLAY-ACTION is called on the
 buffer. Interactively, DISPLAY-ACTION defaults to using
 `switch-to-buffer' unless the intended buffer is already visible
-in some window, in which case that window is selected."
+in some window, in which case that window is selected.
+
+With a prefix arg always prompt for a new connection.
+If arg is `-' prompt for the lisp program and start a new connection. With a
+C-u prefix arg of (4), connect to a running slime. With any other prefix arg,
+prompt and start a lisp from sly-lisp-implementations.
+
+Set `sly-auto-start' to start a lisp if there is no connection.
+"
   (interactive (list (lambda (buf)
                        (let ((w (get-buffer-window buf 0)))
                          (if w (progn (make-frame-visible (window-frame w))
@@ -989,11 +997,16 @@ in some window, in which case that window is selected."
 				      (select-frame-set-input-focus (window-frame w)))
 			   (switch-to-buffer buf))))))
   (let* ((default-directory "~")
-	 (buffer
-          (sly-mrepl--find-create (sly-current-connection))))
-    (when display-action
-      (funcall display-action buffer))
-    buffer))
+	 (interactive-p (called-interactively-p 'any )))
+    ;; (call-interactively-p retard-p retard! edebug retard!)
+    (cond ((and interactive-p (equal current-prefix-arg '(4)))
+	   (call-interactively 'sly-connect))
+	  ((and interactive-p current-prefix-arg)
+	   (call-interactively 'sly nil (this-command-keys-vector)))
+	  (t (let ((buffer
+		    (sly-mrepl--find-create (sly-current-connection))))
+	       (when display-action
+		 (funcall display-action buffer)))))))
 
 (defun sly-mrepl-on-connection ()
   (let* ((inferior-buffer
@@ -1006,7 +1019,9 @@ in some window, in which case that window is selected."
       (sly-mrepl 'pop-to-buffer))
     (when inferior-window
       (bury-buffer inferior-buffer)
-      (delete-window inferior-window))
+      (with-selected-window inferior-window
+	(when (not (one-window-p))
+	  (delete-window inferior-window))))
     (goto-char (point-max))))
 
 (defun sly-mrepl-new (connection &optional handle)
