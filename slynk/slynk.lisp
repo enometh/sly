54569976f05fc9b41ad3ba32cc680356b8e91c6a
Madhu <enometh@net.meer>
2022-07-19 00:15:44 +0530

    slynk/slynk.lisp: (macro-indentation): try indenting &rest same as &body

--- slynk/slynk.lisp
+++ slynk/slynk.lisp
@@ -4040,7 +4040,8 @@ in Emacs."
 
 (defun macro-indentation (arglist)
   (if (well-formed-list-p arglist)
-      (position '&body (remove '&optional (clean-arglist arglist)))
+      (or (position '&body (remove '&optional (clean-arglist arglist)))
+	  (position '&rest (remove '&optional (clean-arglist arglist))))
       nil))
 
 (defun clean-arglist (arglist)
