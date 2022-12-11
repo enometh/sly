a9882641450dd914d2f841443e1a5d2bf401bd8e
Madhu <enometh@net.meer>
2022-12-11 09:37:28 +0530

    slynk/slynk.lisp: (close-channel): fix method signature with &allow-other-keys for some complaining lisps

--- slynk/slynk.lisp
+++ slynk/slynk.lisp
@@ -521,7 +521,7 @@ corresponding values in the CDR of VALUE."
 (defun channel-thread-id (channel)
   (slynk-backend:thread-id (channel-thread channel)))
 
-(defmethod close-channel (channel &key)
+(defmethod close-channel (channel &key &allow-other-keys)
   (let ((probe (find-channel (channel-id channel))))
     (cond (probe (setf (channels) (delete probe (channels))))
           (t (error "Can't close invalid channel: ~a" channel)))))
