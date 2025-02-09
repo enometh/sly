6b3406dcb9d422cc587c701d9920930abed49e85
Madhu <enometh@net.meer>
2025-02-09 21:12:09 +0530

    sly.el: sly-apropos: fill in intial values from symbol-at-point
    * sly.el: (sly-apropos, sly-apropos-all): call
    sly-read-from-minibuffer with initial-value set
    to (sly-symbol-at-point).

--- sly.el
+++ sly.el
@@ -4706,19 +4706,19 @@ arg, you're interactively asked for parameters of the search.
 With M-- (negative) prefix arg, prompt for package only. "
   (interactive
    (cond ((eq '- current-prefix-arg)
-          (list (sly-read-from-minibuffer "Apropos external symbols: ")
+          (list (sly-read-from-minibuffer "Apropos external symbols: " (sly-symbol-at-point t))
                 t
                 (sly-read-package-name "Package (blank for all): "
-                                       nil 'allow-blank)
+				       (sly-current-package) 'allow-blank)
                 nil))
          (current-prefix-arg
-          (list (sly-read-from-minibuffer "Apropos: ")
+          (list (sly-read-from-minibuffer "Apropos: " (sly-symbol-at-point t))
                 (sly-y-or-n-p "External symbols only? ")
                 (sly-read-package-name "Package (blank for all): "
-                                       nil 'allow-blank)
+                                       (sly-current-package)'allow-blank)
                 (sly-y-or-n-p "Case-sensitive? ")))
          (t
-          (list (sly-read-from-minibuffer "Apropos external symbols: ") t nil nil))))
+          (list (sly-read-from-minibuffer "Apropos external symbols: " (sly-symbol-at-point t)) t nil nil))))
   (sly-eval-async
       `(slynk-apropos:apropos-list-for-emacs ,string ,only-external-p
                                              ,case-sensitive-p ',package)
@@ -4729,7 +4729,7 @@ With M-- (negative) prefix arg, prompt for package only. "
 (defun sly-apropos-all ()
   "Shortcut for (sly-apropos <string> nil nil)"
   (interactive)
-  (sly-apropos (sly-read-from-minibuffer "Apropos all symbols: ") nil nil))
+  (sly-apropos (sly-read-from-minibuffer "Apropos all symbols: " (sly-symbol-at-point t)) nil nil))
 
 (defun sly-apropos-package (package &optional internal)
   "Show apropos listing for symbols in PACKAGE.
