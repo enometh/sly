adaf42ffb2f1aca91c033ebb3e676f0cf0a28359
Madhu <enometh@net.meer>
2025-05-11 05:33:57 +0530

    sly.el: make xref support multiple frames, and reuse frames
    * sly.el: (sly--display-source-location): if method is 'frame, use
    display-buffer-other-frame explicitly, instead of relying on
    pop-up-frames behaviour. for the default case, change the call to
    `switch-to-buffer' to `xref--switch-to-buffer'.
    
    (For sanity the user should also advice xref--switch-to-buffer to
    invoke pop-to-buffer-same-window instead of pop-to-buffer and
    customize it to resue frames)

--- sly.el
+++ sly.el
@@ -3772,8 +3772,7 @@ new window or frame."
         (cond ((eq method 'window)
                (display-buffer target-buffer t))
               ((eq method 'frame)
-               (let ((pop-up-frames t))
-                 (display-buffer target-buffer t)))
+              (display-buffer-other-frame target-buffer))
               ((consp method)
                (let* ((window (car method))
                       (sub-method (cdr method)))
@@ -3813,7 +3812,7 @@ new window or frame."
                                 (set-window-buffer window buffer)
                                 window))))))))
               (t
-               (switch-to-buffer target-buffer)
+               (xref--switch-to-buffer target-buffer)
                (selected-window)))))
     (when (eq method 'sly-xref)
       (setq method sly-xref--popup-method))
